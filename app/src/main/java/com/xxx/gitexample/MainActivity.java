a8d34b8dcb04899082ebc9664aacfad5816a996d
Knuckles <knuckles@disp-tech.com>
2017-11-14 21:16:15 +0800

    測試看看修改後再 Commit

--- app/src/main/java/com/xxx/gitexample/MainActivity.java
+++ app/src/main/java/com/xxx/gitexample/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String test = "測試";
+    int xxx = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
