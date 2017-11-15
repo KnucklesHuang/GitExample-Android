7467ee705b357e6f0f95cb8ece1e3297efbee9da
Knuckles <knuckles@disp-tech.com>
2017-11-16 02:03:18 +0800

    新增函數 testBranch()

--- app/src/main/java/com/xxx/gitexample/MainActivity.java
+++ app/src/main/java/com/xxx/gitexample/MainActivity.java
@@ -1,11 +1,16 @@
 package com.xxx.gitexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     String test = "測試";
     int xxx = 1234;
+
+    void testBranch() {
+        Log.d("test","測試分支功能");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
