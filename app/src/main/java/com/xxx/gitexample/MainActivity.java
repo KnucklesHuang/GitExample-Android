6c8b66ad75647dad7f4fde547c22ab6aa12150a3
Knuckles <knuckles@disp-tech.com>
2017-11-16 02:19:25 +0800

    在分支 master 加上函數 testMaster()

--- app/src/main/java/com/xxx/gitexample/MainActivity.java
+++ app/src/main/java/com/xxx/gitexample/MainActivity.java
@@ -1,11 +1,16 @@
 package com.xxx.gitexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     String test = "測試";
     int xxx = 1234;
+
+    void testMaster() {
+        Log.d("test","測試 master 功能");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
