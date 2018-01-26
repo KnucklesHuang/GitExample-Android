d1d0df2a8a9ed5fbd4001498ca5ec20ea7990bcf
Knuckles <knuckles@disp-tech.com>
2018-01-27 04:43:02 +0800

    測試用另一個使用者上傳程式

--- app/src/main/java/com/xxx/gitexample/MainActivity.java
+++ app/src/main/java/com/xxx/gitexample/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d("test","測試用push上傳程式");
     }
 
+    void testNewUserPush() {
+        Log.d("test","測試用另一個使用者上傳程式");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
